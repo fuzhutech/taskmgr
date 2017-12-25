d34718ca5ca304562a44e3beb42a1af96a23fbad
fuzhutech <fuzhutech@163.com>
2017-12-25 22:36:28 +0800

    6-9

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,5 +1,9 @@
-import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
-import {FormGroup, Validators, FormBuilder} from '@angular/forms';
+import {Component, OnInit, OnDestroy, ChangeDetectionStrategy} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+import {extractInfo, getAddrByCode, isValidAddr} from '../../utils/identity.util';
+import {isValidDate, toDate} from '../../utils/date.util';
 
 @Component({
     selector: 'app-register',
@@ -7,11 +11,13 @@ import {FormGroup, Validators, FormBuilder} from '@angular/forms';
     styleUrls: ['./register.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
     selectedTab = 0;
     form: FormGroup;
 
     items: string[];
+    avatars$: Observable<string[]>;
+    private _sub: Subscription;
     private readonly avatarName = 'avatars';
 
     constructor(private fb: FormBuilder) {
@@ -32,6 +38,30 @@ export class RegisterComponent implements OnInit {
             address: ['', Validators.maxLength(80)],
             identity: []
         });
+
+        const id$ = this.form.get('identity').valueChanges
+            .debounceTime(300)
+            .filter(v => this.form.get('identity').valid);
+
+        this._sub = id$.subscribe(id => {
+            const info = extractInfo(id.identityNo);
+            if (isValidAddr(info.addrCode)) {
+                const addr = getAddrByCode(info.addrCode);
+                this.form.patchValue({address: addr});
+                this.form.updateValueAndValidity({onlySelf: true, emitEvent: true});
+            }
+            if (isValidDate(info.dateOfBirth)) {
+                const date = info.dateOfBirth;
+                this.form.patchValue({dateOfBirth: date});
+                this.form.updateValueAndValidity({onlySelf: true, emitEvent: true});
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        if (this._sub) {
+            this._sub.unsubscribe();
+        }
     }
 
     onSubmit({value, valid}, e: Event) {
